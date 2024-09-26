4c3a7bf50df4d7c682c2aac37e490414b6ba0030
Varun Gonsalves <varunbgonsalves@gmail.com>
2024-09-27 00:23:48 +0100

    Gazebo Working with the tutorial Robot and some movement

--- catkin_ws/build/client_server/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/innovation/github/RosandGazebo/catkin_ws/build/client_server/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/pub_sub/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/innovation/github/RosandGazebo/catkin_ws/build/pub_sub/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
