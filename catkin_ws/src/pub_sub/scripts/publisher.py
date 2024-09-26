78f6eb027ef475d39f0c67227f3a075fcdf62616
Varun Gonsalves <varunbgonsalves@gmail.com>
2024-09-26 20:08:33 +0100

    This update has understanding about ros parameter server as well as rosbags

--- catkin_ws/src/pub_sub/scripts/publisher.py
+++ catkin_ws/src/pub_sub/scripts/publisher.py
@@ -5,10 +5,13 @@ from std_msgs.msg import Int64
 
 # Node + Topic Initialization
 rospy.init_node("publisher_node")
-pub = rospy.Publisher(name="topic", data_class=Int64, queue_size=1)
+pub = rospy.Publisher(name="/sam", data_class=Int64, queue_size=1)
+
+pub2 = rospy.Publisher(name="/jenny", data_class=Int64, queue_size=1)
 
 
 # Looping
 while not rospy.is_shutdown():
     pub.publish(1)
+    pub2.publish(2)
     rospy.sleep(1)
